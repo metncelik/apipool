569f24f31b18e45d65c281870b48dd1611110d1d
Metin Çelik <metin5celik@gmail.com>
2024-06-14 17:09:43 +0300

    .

--- src/components/APIList.js
+++ src/components/APIList.js
@@ -3,9 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { APIListSkeleton } from "./sekeleton/APIListSkeleton";
 
 const APIList = ({ apis }) => {
-    
-    if (!apis) return <APIListSkeleton/>;
-    apis = [...apis,...apis, ...apis, ...apis, ...apis, ...apis, ...apis, ...apis ]
+
+    if (!apis) return <APIListSkeleton />;
     return (
         <div className="apis-container">
             <ul className="apis-list">
@@ -18,26 +17,26 @@ const APIList = ({ apis }) => {
                     apis.map(api => (
                         <li key={api.api_id} className="api-item">
                             <Link to={"/api/" + api.alias}>
-                            <img
-                                src={api.image_url}
-                                alt={api.api_title}
-                                className='api-image'
-                                loading="lazy"
-                            />
+                                <img
+                                    src={api.image_url}
+                                    alt={api.api_title}
+                                    className='api-image'
+                                    loading="lazy"
+                                />
 
-                            <div className="api-info">
-                                <div className="api-info-title">
-                                    <div className="api-name">
-                                        {api.api_title}
+                                <div className="api-info">
+                                    <div className="api-info-title">
+                                        <div className="api-name">
+                                            {api.api_title}
+                                        </div>
                                     </div>
+                                    <div className="api-divider"></div>
+
+
+                                    <p className="api-description">
+                                        {api.description.slice(0, 110) + (api.description.length > 100 ? "..." : "")}
+                                    </p>
                                 </div>
-                                <div className="api-divider"></div>
-
-
-                                <p className="api-description">
-                                    {api.description.slice(0, 110) + (api.description.length > 100 ? "..." : "")}
-                                </p>
-                            </div>
                             </Link>
                         </li>
                     )))}
